7830d6cef72115a9fc075899044227548014abd9
chingaloEric <echingalo@gmail.com>
2019-12-10 09:12:49 +0300

    feat: added infinite scroll

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,10 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  dataList: any;
 
-  constructor() {}
+  constructor() {
+    this.dataList = [];
 
+    for (let i = 0; i < 25; i++) {
+      this.dataList.push('Item number ' + this.dataList.length);
+    }
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      for (let i = 0; i < 25; i++) {
+        this.dataList.push('Item number ' + this.dataList.length);
+      }
+      event.target.complete();
+
+      if (this.dataList.length === 50) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
 }
