e68a90f07d67fd0e983a9420fdb4f93c8a53ca84
chingaloEric <echingalo@gmail.com>
2020-04-27 20:24:32 +0300

    feat: 🎸 added a form for adding songs

--- src/app/home/pages/home/home.page.ts
+++ src/app/home/pages/home/home.page.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { DbService } from '../../services/db.service';
+import { ToastController } from '@ionic/angular';
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-  dataList: any;
+export class HomePage implements OnInit {
+  songFormGroup: FormGroup;
+  songFormData: any;
 
-  constructor() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private dbService: DbService,
+    private toast: ToastController
+  ) {}
+
+  ngOnInit() {
+    this.songFormData = {
+      inputs: [
+        { label: 'Artist', formControlName: 'artist', type: 'text' },
+        { label: 'Song', formControlName: 'song', type: 'text' },
+      ],
+    };
+    this.generateEmptyForm();
+  }
+
+  generateEmptyForm() {
+    this.songFormGroup = this.formBuilder.group({});
+    _.forEach(this.songFormData.inputs, (input: any) => {
+      this.songFormGroup.addControl(
+        input.formControlName,
+        new FormControl('', Validators.required)
+      );
+    });
+  }
 }
