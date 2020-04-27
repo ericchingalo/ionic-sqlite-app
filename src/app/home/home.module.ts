eec68a285068e0bd38993fe3dd156f578b8b5bbd
chingaloEric <echingalo@gmail.com>
2020-04-27 14:01:37 +0300

    style: 💄 restrucutred home module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,7 +4,7 @@ import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
-import { HomePage } from './home.page';
+import { HomePage } from './pages/home/home.page';
 
 @NgModule({
   imports: [
@@ -14,10 +14,10 @@ import { HomePage } from './home.page';
     RouterModule.forChild([
       {
         path: '',
-        component: HomePage
-      }
-    ])
+        component: HomePage,
+      },
+    ]),
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
 })
 export class HomePageModule {}
